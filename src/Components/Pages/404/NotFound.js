58b5456c8c5cd76ac1a5c57f12552fdeb18f8ed6
raihankabir36850 <raihanku320@gmail.com>
2023-06-12 12:21:09 +0600

    404 page create

---
+++ src/Components/Pages/404/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound() {
+  return <>Oops You are lost now.</>;
+}
+
+export default NotFound;
--- src/Components/Routes/AllRoutes.js
+++ src/Components/Routes/AllRoutes.js
@@ -4,10 +4,12 @@ import Login from "../Pages/Login/Login";
 import Signup from "../Pages/Signup/Signup";
 import Quiz from "../Pages/Quiz/Quiz";
 import topics from "../../Asset/Data/Topics";
+import NotFound from "../Pages/404/NotFound";
 
 function AllRoutes() {
   return (
     <Routes>
+      <Route path="*" element={<NotFound />} />
       <Route exact path="/" element={<Home />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/signup" element={<Signup />} />
